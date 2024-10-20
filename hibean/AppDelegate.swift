f8ec3f7124bfdc39edbc86ace08ab69406096344
Armond Schneider <mondoloves@icloud.com>
2024-10-19 18:44:38 -0700

    first commit

---
+++ hibean/AppDelegate.swift
@@ -0,0 +1,7 @@
+//
+//  AppDelegate.swift
+//  hibean
+//
+//  Created by Armond Schneider on 10/19/24.
+//
+
---
+++ hibean/ChatAssistantApp.swift
@@ -0,0 +1,19 @@
+//
+//  ChatAssistantApp.swift
+//  hibean
+//
+//  Created by Armond Schneider on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatAssistantApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    var body: some Scene {
+        WindowGroup {
+            EmptyView() 
+        }
+    }
+}
