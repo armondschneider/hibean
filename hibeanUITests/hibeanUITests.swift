d2128b4b76f1820040d8bd63539f469fa78668d5
Armond Schneider <mondoloves@icloud.com>
2024-10-19 18:54:09 -0700

    test work

--- hibeanUITests/hibeanUITests.swift
+++ hibeanUITests/hibeanUITests.swift
@@ -20,6 +20,7 @@ final class hibeanUITests: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        //
     }
 
     @MainActor
